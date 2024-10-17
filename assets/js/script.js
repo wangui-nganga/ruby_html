2b6c818a47e54af9e65762628d9901e55704dce3
wanguinganga <milkahwangui200@gmail.com>
2024-10-17 09:00:14 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+
+const colorButton=document.getElementById('colorButton');
+
+colorButton.addEventListener('click',()=>{
+
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor==='blue' ?'#4CAF50':'blue';
+
+
+});
